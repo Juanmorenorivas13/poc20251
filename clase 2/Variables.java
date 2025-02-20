a17863d0a0545056d0551d6df69a90668cd99495
Juanmorenorivas13 <juan.morenorivas@campusucc.edu.co>
2025-02-20 22:04:18 +0000

    Clase 2

---
+++ clase 2/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+    // metodo main
+    public static void main(String[] args) {
+
+        // tipo de datos
+        // enteros int , long
+        int edad = 25;
+        // Reales double, float
+        double altura = 1.65;
+        // Cadenas
+        String nombre = "juan";
+        // logicos
+        boolean esEstudiantes = true; // false
+        System.out.println("nombre: " + nombre + ",edad:" + edad + ",altura:" + altura + ",estudiantes:"
+                + esEstudiantes);
+    }
+}
