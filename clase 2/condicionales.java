71cf99b8165bb6570a3338af8c5139910fa7417d
Juanmorenorivas13 <juan.morenorivas@campusucc.edu.co>
2025-02-20 22:57:57 +0000

    se creo condicionales.java

--- clase 2/condicionales.java
+++ clase 2/condicionales.java
@@ -1,3 +1,15 @@
 public class condicionales {
-    
+
+    public static void main(String[] args) {
+        int numero = -5;
+
+        if (numero > 0) {
+            System.out.println("el numero es positivo");
+        } else if (numero < 0) {
+            System.out.println("el numero es negativo");
+        } else {
+            System.out.println("el numero es cero");
+        }
+    }
+
 }
